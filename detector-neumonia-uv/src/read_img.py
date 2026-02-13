87530cbac9342758857ab2e1cf64d672f3db813d
soporteERP <soporte.erp2@plataforma.com.co>
2026-02-13 00:30:55 -0500

    refactor: simplify ImageLoader class by removing static methods for reading images

--- detector-neumonia-uv/src/read_img.py
+++ detector-neumonia-uv/src/read_img.py
@@ -1,39 +1,37 @@
 import cv2
 import pydicom as dicom
+import numpy as np
 from PIL import Image
 
 
 class ImageLoader:
     """
-    Clase encargada ÚNICAMENTE de la carga de imágenes desde el sistema de archivos.
-    Soporta formatos DICOM y JPG/PNG/JPEG.
+    Clase encargada de la carga de imágenes desde el sistema de archivos.
+    Soporta formatos DICOM.
     """
 
-    @staticmethod
-    def read_dicom_file(path):
+    def __init__(self,path):
         """
-        Lee un archivo DICOM y devuelve el pixel_array original.
+        Lee un archivo DICOM.
         """
-        img = dicom.dcmread(path)
-        return img.pixel_array
-
-    @staticmethod
-    def read_jpg_file(path):
-        """
-        Lee un archivo de imagen estándar (JPG/PNG) usando OpenCV.
-        Devuelve la imagen en formato BGR (default de OpenCV).
-        """
-        img = cv2.imread(path)
-        if img is None:
-            raise ValueError(f"No se pudo cargar la imagen en la ruta: {path}")
-        return img
-
-    @classmethod
-    def load(cls, path):
-        """
-        Método principal para cargar una imagen basado en su extensión.
-        """
-        if path.lower().endswith(('.dcm', '.dicom')):
-            return cls.read_dicom_file(path)
-        else:
-            return cls.read_jpg_file(path)
+        self.img = dicom.dcmread(path)
+        self._generate_img_to_show()
+        self._generate_img_RGB()
+    
+    def _generate_img_to_show(self):
+        img_array = self.img.pixel_array
+        self.img2show = Image.fromarray(img_array)
+    
+    def _generate_img_RGB(self):
+        img_array = self.img.pixel_array
+        img2 = img_array.astype(float)
+        img2 = (np.maximum(img2, 0) / img2.max()) * 255.0
+        img2 = np.uint8(img2)
+        img_RGB = cv2.cvtColor(img2, cv2.COLOR_GRAY2RGB)
+        self.img_RGB = img_RGB
+    
+    def get_img_RGB(self):
+        return self.img_RGB
+    
+    def get_img_to_show(self):
+        return self.img2show
